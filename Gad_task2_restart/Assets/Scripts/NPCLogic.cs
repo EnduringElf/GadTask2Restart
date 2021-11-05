ea6f718777b545c904dfdec5e5d61fd55bd5745e
Ockert Michiel van Wyk <20109982@vegaconnect.co.za>
2021-11-05 03:43:07 +0200

    Added the MechAnim System as well as the NavMesh System

---
+++ Gad_task2_restart/Assets/Scripts/NPCLogic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCLogic : MonoBehaviour
+{
+
+    private Animator npcAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        npcAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
